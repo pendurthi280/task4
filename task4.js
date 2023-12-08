f95f72da41bc997047460e08a6c69a2c2ad6c366
pendurthi280 <151996518+pendurthi280@users.noreply.github.com>
2023-12-08 12:36:23 +0530

    Add files via upload
    task4

---
+++ task4.js
@@ -0,0 +1,105 @@
+function validategender()
+ {
+    let x = document.forms["form"]["gender"].value;
+    if (x == "male") 
+    {
+      
+       alert("Hi sir");
+    }
+    else
+    {
+        alert(" Hi Madam");
+    }
+
+ }
+
+
+function validate()
+{
+   var username = document.getElementById("name");
+   var useremail = document.getElementById("email");
+   var  Organization = document.getElementById("organization");
+   if(username.value=="")
+   {
+     document.getElementById("error").innerHTML ="enter a name";
+   }
+   if(useremail .value=="")
+   {
+     document.getElementById("error1").innerHTML ="enter a email";
+   }
+   if(Organization.value=="")
+   {
+     document.getElementById("error2").innerHTML ="enter name of org";
+   }
+}
+
+/*function clearform()
+     {
+        var username = document.getElementById("name");
+        var useremail = document.getElementById("email");
+        var  Organization = document.getElementById("organization");
+        var  phone = document.getElementById("phone");
+        var  city= document.getElementById("city");
+        var  code= document.getElementById("code");
+        var  address= document.getElementById("address");
+        var aboutorgnization = document.getElementById("aboutorgnization");
+        var  textarea = document.getElementById("textarea");
+         username.value = "";
+         useremail.value = "";
+         Organization.value = "";
+         phone.value = "";
+         city.value="";
+         code.value="";
+         address.value="";
+         aboutorgnization.value="";
+         textarea.value="";
+         if( document.getElementById("error").innerHTML == "enter a name"||document.getElementById("error1").innerHTML == "enter a email"||document.getElementById("error3").innerHTML == "enter name of org")
+         {
+          document.getElementById("error").innerHTML = "*";
+          document.getElementById("error1").innerHTML = "*";
+          document.getElementById("error2").innerHTML = "*";
+         }
+         var ele = document.getElementsByName("contact1");
+         for(var i=0;i<ele.length;i++)
+             ele[i].checked = false;
+      }*/
+
+
+  function clearform()
+      {
+        document.getElementById("form").reset();
+        if( document.getElementById("error").innerHTML == "enter a name"||document.getElementById("error1").innerHTML == "enter a email"||document.getElementById("error3").innerHTML == "enter name of org")
+         {
+          document.getElementById("error").innerHTML = "*";
+          document.getElementById("error1").innerHTML = "*";
+          document.getElementById("error2").innerHTML = "*";
+         }
+      
+      }
+
+
+
+
+
+     /*function function1()
+     {
+     let inputs = document.getElementsByTagName('input');
+      for(i = 0; i <inputs.length; i++) 
+      {
+          inputs.value="";
+      }
+    }*/
+
+   
+    /*var inputs=document.querySelectorAll('input');*/
+    
+    
+
+
+
+    function promo(){
+      
+      var state = document.getElementById("dropdown").value;
+      var promo = state + " - PROMO";
+      document.getElementById("promotext").innerHTML = promo;
+    }
